03098646d38aeedc8bd74be84ac2421f33328ea7
raidou12 <icarodaviduarte@gmail.com>
2019-04-19 19:00:26 -0300

    feat: add Watson natural language undersanding service

--- credentials/README.md
+++ credentials/README.md
@@ -6,6 +6,21 @@ File : `algoritmia.json`
 
 ```
 {
-    "apiKey": "create acount algoritmia and put your key"
+    "apiKey": "create account algoritmia and put your key"
+}
+```
+
+## Watson Natural Language Understanding
+
+File: `watson-nlu.json`
+
+```
+{
+  "apikey": "create account into IBM Clound and get your json credentials",
+  "iam_apikey_description": "",
+  "iam_apikey_name": "",
+  "iam_role_crn": "",
+  "iam_serviceid_crn": "",
+  "url": ""
 }
 ```
--- robots/text.js
+++ robots/text.js
@@ -2,16 +2,27 @@ const algorithmia = require('algorithmia');
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey;
 const setenceBoundatyDetection = require('sbd');
 
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey;
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+
+const nlu = new NaturalLanguageUnderstandingV1({
+    version: '2018-04-05',
+    url: 'https://gateway.watsonplatform.net/natural-language-understanding/api',
+    iam_apikey: watsonApiKey,
+});
+
 async function robot(content){
     await fetchContentFromWikipedia(content);
     sanitizeContent(content);
     breakContentIntoSentences(content);
+    limitMaximumSentences(content);
+    await fetchKeywordsOfAllSentences(content);
 
     async function fetchContentFromWikipedia(content){
         const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey);
         const wikipediaAlgoritm = algorithmiaAuthenticated.algo("web/WikipediaParser/0.1.2");
-        const wikipediaResponde = await wikipediaAlgoritm.pipe(content.searchTerm);
-        const wikipediaContent = wikipediaResponde.get();
+        const wikipediaResponse = await wikipediaAlgoritm.pipe(content.searchTerm);
+        const wikipediaContent = wikipediaResponse.get();
         
         content.sourceContentOriginal = wikipediaContent.content;
     }
@@ -41,17 +52,48 @@ async function robot(content){
     }
 
     function breakContentIntoSentences(content){
-        content.setences = [];
+        content.sentences = [];
 
         const sentences = setenceBoundatyDetection.sentences(content.sourceContentSanitized);
         sentences.forEach((sentence)=>{
-            content.setences.push({
+            content.sentences.push({
                 text: sentence,
                 keywords: [],
                 images: []
             })
         });
     }
+    
+    function limitMaximumSentences(content){
+        content.sentences = content.sentences.slice(0, content.maximumSentences);
+    }
+
+    async function fetchKeywordsOfAllSentences(content){
+        for (const sentence of content.sentences){
+            sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text);
+        }
+    }
+
+    async function fetchWatsonAndReturnKeywords(setence){
+        return new Promise((resolve, reject)=>{
+            nlu.analyze({
+                text: setence,
+                features: {
+                    keywords : {}
+                }
+            },(error, response)=>{
+                if(error){
+                    throw error;
+                }
+    
+                const keywords = response.keywords.map((keyword) => keyword.text);
+    
+                resolve(keywords)
+            }); 
+        });
+    }
+
 }
+
 
 module.exports = robot; 
